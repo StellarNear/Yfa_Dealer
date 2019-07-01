ab6ded3d0c1a0faba307a0b287d9a06e1914468d
jchatron <jchatron@letsignit.com>
2019-07-01 15:22:17 +0200

    fix spend simultané

--- app/src/main/java/stellarnear/yfa_dealer/SliderBuilder.java
+++ app/src/main/java/stellarnear/yfa_dealer/SliderBuilder.java
@@ -13,6 +13,7 @@ public class SliderBuilder {
     private Context mC;
     private Calculation calculation=new Calculation();
     private OnCastEventListener mListener;
+    private SeekBar seek;
     private Tools tools=new Tools();
 
     public SliderBuilder(Context mC,Spell spell){
@@ -21,28 +22,23 @@ public class SliderBuilder {
     }
 
     public void setSlider(final SeekBar seek){
+        this.seek=seek;
         seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 if (seekBar.getProgress() > 75) {
                     seekBar.setProgress(100);
-                    if (spell.getRank() == 0 || yfa.getResourceValue("spell_rank_" + calculation.currentRank(spell)) > 0) {
-                        if (spell.getRank() != 0) {
-                            yfa.castSpell(spell);
-                        }
-                        if(!spell.getConversion().getArcaneId().equalsIgnoreCase("")){
-                            yfa.castConvSpell(spell.getConversion().getRank());
-                        }
-                        if(spell.elementIsConverted()){
-                            yfa.getAllResources().getResource("mythic_points").spend(1);
-                            tools.customToast(mC, "Il te reste " + yfa.getResourceValue("mythic_points") + " point(s) mythique(s)", "center");
-                        }
-                        mListener.onEvent();
-                        Snackbar.make(seek, "Lancement du sort : " + spell.getName(), Snackbar.LENGTH_LONG)
-                                .setAction("Action", null).show();
-                    } else {
+                    if (spell.getRank() != 0 && yfa.getResourceValue("spell_rank_" + calculation.currentRank(spell)) <= 0) {
                         seekBar.setProgress(1);
-                        tools.customToast(mC,"Tu n'as pas d'emplacement de sort "+calculation.currentRank(spell)+" de disponible...");
+                        tools.customToast(mC,"Tu n'as pas d'emplacement de sort "+calculation.currentRank(spell)+" de disponible...","center");
+                    } else if (!spell.getConversion().getArcaneId().equalsIgnoreCase("") && yfa.getResourceValue("spell_conv_rank_" + spell.getConversion().getRank())<=0) {
+                        seekBar.setProgress(1);
+                        tools.customToast(mC, "Tu n'as pas d'emplacement de sort convertible " + calculation.currentRank(spell) + " de disponible...","center");
+                    } else if (spell.elementIsConverted() && yfa.getResourceValue("mythic_points") <= 0) {
+                        seekBar.setProgress(1);
+                        tools.customToast(mC, "Il ne te reste aucun point mythique ","center");
+                    } else {
+                        startCasting();
                     }
                 } else {
                     seekBar.setProgress(1);
@@ -64,6 +60,22 @@ public class SliderBuilder {
                 }
             }
         });
+    }
+
+    private void startCasting() {
+        if (spell.getRank() != 0) {
+            yfa.castSpell(spell);
+        }
+        if(!spell.getConversion().getArcaneId().equalsIgnoreCase("")){
+            yfa.castConvSpell(spell.getConversion().getRank());
+        }
+        if(spell.elementIsConverted()){
+            yfa.getAllResources().getResource("mythic_points").spend(1);
+            tools.customToast(mC, "Il te reste " + yfa.getResourceValue("mythic_points") + " point(s) mythique(s)", "center");
+        }
+        mListener.onEvent();
+        Snackbar.make(seek, "Lancement du sort : " + spell.getName(), Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
 
     public interface OnCastEventListener {
